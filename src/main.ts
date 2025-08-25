20b27dbe158c2faf383f0f38433b223444b034c6
Illia Stadnyk <ilya.stadnyk@gmail.com>
2025-08-25 21:28:09 +0200

    refactor final project

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,8 @@ async function bootstrap() {
   await app.enableShutdownHooks();
 
   const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
+  await app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
 }
 bootstrap();
